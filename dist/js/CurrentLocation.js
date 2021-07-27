fa0e4c5fb7244ed84270242bd9e2338f979447a3
Bobnini <guilhermegcadilhe@gmail.com>
2021-07-26 21:49:17 -0300

    Starting JS

---
+++ dist/js/CurrentLocation.js
@@ -0,0 +1,8 @@
+export default class CurrentLocation {
+  constructor() {
+    this._name = 'Local Atual';
+    this._lat = null;
+    this._lon = null;
+    this._unit = 'metric';
+  }
+}
---
+++ dist/js/main.js
@@ -0,0 +1,21 @@
+const initApp = () => {
+  // add listeners
+  // set up
+  // load weather
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
+
+//   <div class="icon" id="icon" style="color: yellow">
+//   <i class="fas fa-sun" aria-hidden="true" title="clear sky"></i>
+// </div>
+// <div class="temp">
+//   59°
+//   <div class="unit">F</div>
+// </div>
+// <div class="desc">Clear Sky</div>
+// <div class="feels">Feels Like 53°</div>
+// <div class="maxtemp">High 62°</div>
+// <div class="mintemp">Low 39°</div>
+// <div class="humidity">Humidity 44%</div>
+// <div class="wind">Wind 7 mph</div>
